6973a545877a7a9185ef80ba757cd4235de08dcd
Jacqueline <jacquelinenyinawabagesera@gmail.com>
2025-04-23 07:27:32 +0300

    answered all the questions

--- test.js
+++
@@ -1,9 +0,0 @@
-function registrationQueue(queue){
-    let currentUser = 0;
-    while(currentUser<(queue.length)){
-   let currentUser = queue.shift();
-   console.log({currentUser});
-    }
-}
-let queue = ["Jacky","Jack","Berissa","Saloi"]
-registrationQueue(queue)
